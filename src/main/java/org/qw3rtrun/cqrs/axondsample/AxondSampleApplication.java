8d47d0816babde711eb70081429a2ce151573fda
Sergey Trunov <sergey.trunov@dataart.com>
2019-06-03 12:58:33 +0300

    tracked event

--- src/main/java/org/qw3rtrun/cqrs/axondsample/AxondSampleApplication.java
+++ src/main/java/org/qw3rtrun/cqrs/axondsample/AxondSampleApplication.java
@@ -1,14 +1,20 @@
 package org.qw3rtrun.cqrs.axondsample;
 
 import org.axonframework.commandhandling.gateway.CommandGateway;
+import org.axonframework.config.EventProcessingConfigurer;
 import org.axonframework.config.ProcessingGroup;
 import org.axonframework.eventhandling.EventHandler;
 import org.axonframework.eventhandling.ResetHandler;
 import org.axonframework.eventhandling.tokenstore.TokenStore;
+import org.axonframework.eventhandling.tokenstore.jdbc.JdbcTokenStore;
+import org.axonframework.eventhandling.tokenstore.jdbc.PostgresTokenTableFactory;
+import org.axonframework.eventhandling.tokenstore.jdbc.TokenTableFactory;
 import org.qw3rtrun.cqrs.axondsample.api.cmd.IntroduceProduct;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +25,22 @@ public class AxondSampleApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AxondSampleApplication.class, args);
+    }
+
+    @Bean
+    public TokenTableFactory tokenTable() {
+        return new PostgresTokenTableFactory();
+    }
+
+    @Bean
+    public CommandLineRunner schemaCreator(TokenStore ts, TokenTableFactory tf) {
+        return (args) -> ((JdbcTokenStore)ts).createSchema(tf);
+    }
+
+    @Autowired
+    public void configure(EventProcessingConfigurer config, TokenStore ts) {
+        config.registerTrackingEventProcessor("logger")
+        .registerTokenStore("logger", c -> ts);
     }
 
 }
